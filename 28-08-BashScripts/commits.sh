660db11fde94cc105f7b862e0b708b7d13b656b5
Maksym Kostenko <kostenko.m.t@gmail.com>
2021-09-02 21:04:58 +0200

    Create commits.sh

---
+++ 28-08-BashScripts/commits.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+for run in {1..10}
+do
+  # shellcheck disable=SC2006
+  now=`date`
+  git add .
+  git commit -m "commit $run"
+  git push --set-upstream origin 28-08-BashScripts
+  echo "Commit number $run. Made - $now"
+done
